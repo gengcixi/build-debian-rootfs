e145fb67b0e95e07847f073397f5f46456aaa06f
gengcixi <gengcixi@163.com>
2018-11-07 13:58:55 +0800

    add make rootfs image

---
+++ mkimg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ARCH=$1
+IMG=rootfs-${ARCH}.img
+
+dd if=/dev/zero of=${IMG} bs=1M count=2500
+mkfs.ext4 ${IMG}
+
+if [-d tmp ];then 
+    rm -rf tmp
+fi
+
+mkdir ./tmp
+mount ${IMG} tmp/
+
+rsync -axHAX --progress rootfs-${ARCH}/* tmp/
+
+umount tmp/
+rm -rf tmp/
+
+e2fsck -p -f ${IMG}
+resize2fs -M ${IMG}
