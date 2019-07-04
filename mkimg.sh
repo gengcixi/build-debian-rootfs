8e3a3f15a829275dcfd1dc70a39328df386843d3
Cixi.Geng <gengcx@spreadst.com>
2019-07-04 17:40:57 +0800

    rewrite mkimg

--- mkimg.sh
+++ mkimg.sh
@@ -6,20 +6,27 @@ if [ -f ${IMG} ];then
     rm ${IMG}
 fi
 
-dd if=/dev/zero of=${IMG} bs=1M count=2500
+echo "making image..."
+dd if=/dev/zero of=${IMG} bs=1M count=1500
 mkfs.ext4 ${IMG}
+mkfs.ext4 rootfs_debian_${ARCH}.ext4
 
 if [-d tmp ];then 
     rm -rf tmp
 fi
 
 mkdir ./tmp
-mount ${IMG} tmp/
+echo "copy data into rootfs..."
+mount -t ext4 ${IMG} tmp/ -o loop
+cp -af rootfs_debian_${ARCH}/* tmpfs/
+
 
 rsync -axHAX --progress rootfs-${ARCH}/* tmp/
 
 umount tmp/
 rm -rf tmp/
 
-e2fsck -p -f ${IMG}
-resize2fs -M ${IMG}
+chmod 777 ${IMG}
+
+#e2fsck -p -f ${IMG}
+#resize2fs -M ${IMG}
