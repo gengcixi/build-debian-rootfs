0ca4acdd2491ba190e2af4586773f67e38a6a521
Cixi.Geng <gengcx@spreadst.com>
2019-07-18 17:36:53 +0800

    add config for sprd wcn & adb

--- ifconfig.sh
+++ ifconfig.sh
@@ -17,6 +17,48 @@ start()
 	echo "Starting usb0"
 	/sbin/ifconfig usb0 192.168.8.8 netmask 255.255.255.0
 	/sbin/route add default gw 192.168.8.1 usb0
+	# mount wcn module data
+	mkdir -p /dev/block/platform/soc/soc:ap-ahb/20600000.sdio/by-name/
+	cd /dev/block/platform/soc/soc\:ap-ahb/20600000.sdio/by-name/
+	wcnmodemPartNo=`fdisk -l |grep "wcnmodem"| awk '{print $1}'`
+	gpsglPartNo=`fdisk -l |grep "gpsgl"| awk '{print $1}'`
+	gpsbdPartNo=`fdisk -l |grep "gpsbd"| awk '{print $1}'`
+	ln -sf /dev/mmcblk0p${wcnmodemPartNo} wcnmodem
+	ln -sf /dev/mmcblk0p${gpsglPartNo} gpsgl
+	ln -sf /dev/mmcblk0p${gpsbdPartNo} gpsbd
+
+	#config usb adbd functions
+        mount -t configfs none /sys/kernel/config
+        sn=`awk '{for(i=1;i<=NF;i++) if($i~/serialno/) print $i}' /proc/cmdline`
+        udc=`ls /sys/class/udc/`
+        sn=${sn#*=}
+
+        mkdir /sys/kernel/config/usb_gadget/g1
+        echo "0x1782" > /sys/kernel/config/usb_gadget/g1/idVendor
+        echo "0x4002" > /sys/kernel/config/usb_gadget/g1/idProduct
+
+        mkdir /sys/kernel/config/usb_gadget/g1/strings/0x409
+        echo $sn > /sys/kernel/config/usb_gadget/g1/strings/0x409/serialnumber
+        echo "Spreadtrum" > /sys/kernel/config/usb_gadget/g1/strings/0x409/manufacturer
+        echo "Spreadtrum Phone" > /sys/kernel/config/usb_gadget/g1/strings/0x409/product
+
+        mkdir -p /sys/kernel/config/usb_gadget/g1/functions/ffs.adb
+        mkdir -p /dev/usb-ffs/adb
+        /bin/mount -t functionfs  adb  /dev/usb-ffs/adb
+
+        mkdir -p /sys/kernel/config/usb_gadget/g1/configs/b.1
+        mkdir -p /sys/kernel/config/usb_gadget/g1/configs/b.1/strings/0x409
+        echo "Conf 1" > /sys/kernel/config/usb_gadget/g1/configs/b.1/strings/0x409/configuration
+        echo "120" > /sys/kernel/config/usb_gadget/g1/configs/b.1/MaxPower
+
+        ln -s /sys/kernel/config/usb_gadget/g1/functions/ffs.adb /sys/kernel/config/usb_gadget/g1/configs/b.1/f1
+        echo "adb" > /sys/kernel/config/usb_gadget/g1/configs/b.1/strings/0x409/configuration
+
+        echo "Starting adbd..."
+        /usr/bin/adbd &
+
+        sleep 1
+        echo "$udc" > /sys/kernel/config/usb_gadget/g1/UDC
 }
 stop()
 {
